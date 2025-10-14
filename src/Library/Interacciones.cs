2c8f6c0a621ff565578d743f1ae7116702ebe436
Andres <ANDRES.RODRIGUEZC@correo.ucu.edu.uy>
2025-10-14 20:13:07 -0300

    trabaje en main, sorry

---
+++ src/Library/Interacciones.cs
@@ -0,0 +1,22 @@
+﻿namespace Library;
+
+public class Interacciones
+{
+    public string Notas { get; set; }
+    public Cliente Cliente { get; set; }
+    public string Tema { get; set; }
+    public string Fecha { get; set; }
+
+    public Interacciones(Cliente cliente, string tema, string fecha)
+    {
+        this.Cliente = cliente;
+        this.Tema = tema;
+        this.Fecha = fecha;
+    }
+
+    public void AgergarNotas(string notas)
+    {
+        this.Notas = notas;
+    }
+    
+}
