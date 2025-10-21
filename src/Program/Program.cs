83e0e6241d23dffc8159b4a24f1733e30377d7ee
Horacio <horacio357diaz@gmail.com>
2025-10-21 09:45:10 -0300

    Formato Program

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -2,12 +2,11 @@
 using System.Collections.Generic;
 namespace Program
 {
-
-class Program
-{
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello, World!");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
     }
-}
 }
