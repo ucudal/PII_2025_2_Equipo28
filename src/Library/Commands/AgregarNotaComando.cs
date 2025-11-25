819f1c2186a0d6ce8ef420755e22b0356c6365a6
Andres263634 <ANDRES.RODRIGUEZC@correo.ucu.edu.uy>
2025-11-25 12:14:02 -0300

    unos comandos mas

---
+++ src/Library/Commands/AgregarNotaComando.cs
@@ -0,0 +1,41 @@
+using System.Threading.Tasks;
+using Discord.Commands;
+using Discord.WebSocket;
+using Library;
+
+
+namespace Ucu.Poo.DiscordBot.Commands
+{
+
+    /// <summary>
+    /// Esta clase agrega una nota a una interaccion.
+    /// Devuele la confirmacion de si se hizo o no.
+    /// </summary>
+// ReSharper disable once UnusedType.Global
+    public class AgregarNotaComando : ModuleBase<SocketCommandContext>
+    {
+        private Fachada fachada = Fachada.Instancia;
+        /// <summary>
+        /// Implementa el comando 'AgregarNota' que agrega una nota a una interaccion. Este comando es para la historia 10.
+        /// </summary>
+        [Command("AgregarNota")]
+        [Summary(
+            "Agrega una nota a una interaccion y devuelve una confirmacion de si se hizo en caso de que asi sea, en caso opuesto devuelve un error y su explicacion")]
+        // ReSharper disable once UnusedMember.Global
+        public async Task ExecuteAsync([Remainder][Summary("xxx")]string parametros)
+        {
+            string[] parte = parametros.Split(',');
+            if (parte.Length != 4)
+            {
+                await ReplyAsync($"Se nececitan 4 parametros. Recurda separar los parametros por ','");
+
+            }
+            else
+            {
+                string mensaje = fachada.AgregarNota(parte[0], parte[1], parte[2], parte[3]);
+                await ReplyAsync($"{mensaje}");
+
+            }
+        }
+    }
+}
---
+++ src/Library/Commands/InteraccionesdeClienteComando.cs
@@ -0,0 +1,41 @@
+using System.Threading.Tasks;
+using Discord.Commands;
+using Discord.WebSocket;
+using Library;
+
+
+namespace Ucu.Poo.DiscordBot.Commands
+{
+
+    /// <summary>
+    /// Esta clase muestra las interacciones de un cliente en base a las parametros.
+    /// Devuele la informacion.
+    /// </summary>
+// ReSharper disable once UnusedType.Global
+    public class InteracciondeClienteComando : ModuleBase<SocketCommandContext>
+    {
+        private Fachada fachada = Fachada.Instancia;
+        /// <summary>
+        /// Implementa el comando 'InteraccionesCliente' que muestra las interacciones de un cliente, en base al tipo, la fecha, ambos o ninguno. Este comando es para la historia ??.
+        /// </summary>
+        [Command("InteraccionesCliente")]
+        [Summary(
+            "Muestra las interacciones en base a los datos proporcionados, en caso opuesto devuelve un error y su explicacion")]
+        // ReSharper disable once UnusedMember.Global
+        public async Task ExecuteAsync([Remainder][Summary("xxx")]string parametros)
+        {
+            string[] parte = parametros.Split(',');
+            if (parte.Length != 4)
+            {
+                await ReplyAsync($"Se nececitan 5 parametros. Recurda separar los parametros por ','");
+
+            }
+            else
+            {
+                string mensaje = fachada.InteraccionesCliente(parte[0], parte[1], parte[2], parte[3]);
+                await ReplyAsync($"{mensaje}");
+
+            }
+        }
+    }
+}
