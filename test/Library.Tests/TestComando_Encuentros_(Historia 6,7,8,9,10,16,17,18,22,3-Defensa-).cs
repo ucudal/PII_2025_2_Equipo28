5d560d8593e49967ec66a6405acec247979a51c7
Andres263634 <ANDRES.RODRIGUEZC@correo.ucu.edu.uy>
2025-12-04 11:27:28 -0300

    ajustes

--- test/Library.Tests/TestComando_Encuentros_(Historia 6,7,8,9,10,16,17,18,22,3-Defensa-).cs
+++ test/Library.Tests/TestComando_Encuentros_(Historia 6,7,8,9,10,16,17,18,22,3-Defensa-).cs
@@ -6,7 +6,7 @@ using System.Text;
 using NUnit.Framework;
 namespace Library.Tests
 {
-    public class TetsComando_Encuentros_6_7_8_9_10_16_17_18_22_
+    public class TetsComando_Encuentros_6_7_8_9_10_16_17_18_22_3Defensa
     {
         private Fachada fachada;
         private Usuario usuario;
@@ -26,6 +26,7 @@ namespace Library.Tests
             fachada.Etiquetas.EliminarDatos();
             fachada.Clientes.EliminarDatos();
             fachada.ClientesContacta.ElminarDatos();
+            fachada.Ventas.EliminarDatos();
             fachada.CrearAdministrador("A1", "Lansaguisantes");
             fachada.CrearUsuario("U1", "Petaseta", "A1");
             fachada.CrearCliente("C1", "Harry", "ElSucioPotter", "099786435", "Harringy@cabezatermo.com");
@@ -134,6 +135,71 @@ namespace Library.Tests
             fachada.EliminarClienteContacto("U1", "C1");
             string resultado = fachada.VerClienteContacto("U1");
             Assert.That(resultado, Does.Not.Contain("Harry ElSucioPotter"));
+        }
+        [Test]
+        public void Comando_verClientesProducto_Correcto()
+        {
+            fachada.CrearAdministrador("A6", "Pepe");
+            fachada.CrearUsuario("U6", "Juan", "A6");
+            fachada.CrearCliente("C4", "Andres", "Perez", "099 298 626", "andres@mail.com");
+            fachada.CrearCliente("C3", "Lucas", "Gonzales", "099 876 321", "Luca@mail.com");
+            fachada.RegistrarVentaCliente("C4", "computadora", "10/11/2025", "300", "U6");
+            fachada.RegistrarVentaCliente("C3", "computadora", "14/11/2025", "300", "U6");
+            string resultado = fachada.VerClientesVentaProducto("computadora", "U6");
+            Assert.That(resultado,Does.Contain("Andres Perez"));
+            Assert.That(resultado,Does.Contain("Lucas Gonzales"));
+        }
+
+        [TestCase ("U9","El usuario no puede ser null (Parameter 'usuario') usuario")]
+        [TestCase ("","El usuario no puede ser null (Parameter 'usuario') usuario")]
+        public void Comando_verClientesProducto_UsuarioIncorrecto(string usuarioId,string esperado)
+        {
+            fachada.CrearAdministrador("A6", "Pepe");
+            fachada.CrearUsuario("U6", "Juan", "A6");
+            fachada.CrearCliente("C4", "Andres", "Perez", "099 298 626", "andres@mail.com");
+            fachada.CrearCliente("C3", "Lucas", "Gonzales", "099 876 321", "Luca@mail.com");
+            fachada.RegistrarVentaCliente("C4", "computadora", "10/11/2025", "300", "U6");
+            fachada.RegistrarVentaCliente("C3", "computadora", "14/11/2025", "300", "U6");
+            string resultado = fachada.VerClientesVentaProducto("computadora", "U9");
+            Assert.That(resultado,Is.EqualTo(esperado));
+        }
+        [Test]
+        public void Comando_verClientesProducto_prodcutoIncorrectoVacio()
+        {
+            fachada.CrearAdministrador("A6", "Pepe");
+            fachada.CrearUsuario("U6", "Juan", "A6");
+            fachada.CrearCliente("C4", "Andres", "Perez", "099 298 626", "andres@mail.com");
+            fachada.CrearCliente("C3", "Lucas", "Gonzales", "099 876 321", "Luca@mail.com");
+            fachada.RegistrarVentaCliente("C4", "computadora", "10/11/2025", "300", "U6");
+            fachada.RegistrarVentaCliente("C3", "computadora", "14/11/2025", "300", "U6");
+            string resultado = fachada.VerClientesVentaProducto("", "U6");
+            string esperado = "El producto no puede ser vacio o null (Parameter 'producto') producto";
+            Assert.That(resultado,Is.EqualTo(esperado));
+        }
+        [Test]
+        public void Comando_verClientesProducto_productoIncorrectoDistinto()
+        {
+            fachada.CrearAdministrador("A6", "Pepe");
+            fachada.CrearUsuario("U6", "Juan", "A6");
+            fachada.CrearCliente("C4", "Andres", "Perez", "099 298 626", "andres@mail.com");
+            fachada.CrearCliente("C3", "Lucas", "Gonzales", "099 876 321", "Luca@mail.com");
+            fachada.RegistrarVentaCliente("C4", "computadora", "10/11/2025", "300", "U6");
+            fachada.RegistrarVentaCliente("C3", "computadora", "14/11/2025", "300", "U6");
+            string resultado = fachada.VerClientesVentaProducto("nada", "U6");
+            Assert.That(resultado,Does.Not.Contain("Andres Perez"));
+            Assert.That(resultado,Does.Not.Contain("Lucas Gonzales"));
+        }
+        [Test]
+        public void Comando_verClientesProducto_ClienteDosveces()
+        {
+            fachada.CrearAdministrador("A6", "Pepe");
+            fachada.CrearUsuario("U6", "Juan", "A6");
+            fachada.CrearCliente("C4", "Andres", "Perez", "099 298 626", "andres@mail.com");
+            fachada.RegistrarVentaCliente("C4", "computadora", "10/11/2025", "300", "U6");
+            fachada.RegistrarVentaCliente("C4", "computadora", "14/11/2025", "300", "U6");
+            string resultado = fachada.VerClientesVentaProducto("computadora", "U6");
+            string esperado = "Los clientes con las ventas de el producto computadora son:\nAndres Perez\n";
+            Assert.That(resultado,Is.EqualTo(esperado));
         }
     }
 }
